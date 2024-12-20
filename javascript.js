9dbddb8052ee3f205902ea4cc8be92b55fc7b475
NateNgu <189771634+NateNgu@users.noreply.github.com>
2024-12-19 20:24:23 -0600

    feat: add functions for basic operations

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+  const total = num1 + num2;
+  return total;
+}
+
+function subtract(num1, num2) {
+  const total = num1 - num2;
+  return total;
+}
+
+function multiply(num1, num2) {
+  const total = num1 * num2;
+  return total;
+}
+
+function divide(num1, num2) {
+  const total = num1 / num2;
+  return total;
+}
