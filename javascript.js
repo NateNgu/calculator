cbed00fc0e7a907a663aac83b35a7affdcb4fa0e
NateNgu <189771634+NateNgu@users.noreply.github.com>
2024-12-19 20:37:58 -0600

    feat: add operator functions and variables

--- javascript.js
+++ javascript.js
@@ -17,3 +17,20 @@ function divide(num1, num2) {
   const total = num1 / num2;
   return total;
 }
+
+let number1;
+let operator;
+let number2;
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "add":
+      return add(num1, num2);
+    case "subtract":
+      return subtract(num1, num2);
+    case "multiply":
+      return multiply(num1, num2);
+    case "divide":
+      return divide(num1, num2);
+  }
+}
